a2e7cd148042f78075a63de21e29fdbf32ab0821
Tony L. Kerz <a716948@MA716948A-HFD-OEM.local>
2016-06-18 10:27:13 -0400

    [dbg] tweak

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import debug from 'debug'
 import React, {Component} from 'react'
-const dbg = debug('app:shared:image')
+const dbg = debug('react-defaulting-image')
 
 export default class extends Component {
 
